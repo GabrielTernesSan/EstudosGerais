50c88d09594273c0318365e872fb7e16bdb2be04
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-05-02 20:48:38 -0300

    Functions

---
+++ Jstack-SQL/Commands.md
@@ -0,0 +1,9 @@
+Docker start (nome do container) // Inicia um container já criado
+
+Docker compose down // Destroi o container 
+
+Docker compose up // Cria o container
+
+Docker exec -it (container) Bash
+
+psql -f (arquivo sql)
--- Jstack-SQL/PT02/scripts/01.sql
+++ Jstack-SQL/PT02/scripts/01.sql
@@ -1,3 +1,24 @@
 \c live024
 
-SELECT *  FROM Customers;
+-- SELECT Id, CONCAT(FirstName, ' ', LastName) AS "FullName"
+-- FROM Customers;
+
+--SELECT *  FROM Orders;
+
+-- SELECT COUNT(*) AS "TotalCustomers"
+-- FROM Customers;
+
+-- SELECT * FROM Customers ORDER BY Email;
+
+-- SELECT COUNT(Id) AS "TotalSales", SUM(Amount) AS "TotalRevenue"
+-- FROM Orders;
+
+-- SELECT Amount FROM Orders ORDER BY Amount DESC LIMIT 1;
+
+SELECT 
+    COUNT(Id) AS "TotalSales", 
+    SUM(Amount) AS "TotalRevenue",
+    MAX(Amount) AS "OrderMoreExpensive",
+    MIN(Amount) AS "OrderLessExpensive",
+    ROUND(AVG(Amount), 2) AS "AvarageTicket"
+FROM Orders;
