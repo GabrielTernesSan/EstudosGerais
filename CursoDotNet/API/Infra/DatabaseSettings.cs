e0cf44dcc7e54c03f4430756d54ed854248ed105
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-05-21 20:47:44 -0300

    Criando o repository

---
+++ CursoDotNet/API/Infra/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Infra
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+    }
+
+    public interface IDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
