c9710d2bb12eafdcbbd3f24ee138d307fbaebf44
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-24 15:56:56 -0300

    Records e imutabilidade

--- Alura/alura-memoria/UsuarioLib/Program.cs
+++ Alura/alura-memoria/UsuarioLib/Program.cs
@@ -6,28 +6,7 @@ Usuario usuario =
         "daniel@email.com",
         new List<string>() {"12345678"});
 
-
-//12345678
-//usuario.ExibeTelefones();
-
-//efetuar a padronização
-//usuario.PadronizaTelefones();
-
-//912345678
-//usuario.ExibeTelefones();
-
-FormularioDto dto = new FormularioDto();
-dto.Nome = "Gabriel";
-dto.Idade = 24;
-dto.Cargo = "Programador Backend";
-dto.Cpf = "11111111111111";
-
-FormularioDto dto2 = new FormularioDto();
-dto2.Nome = "Gabriel";
-dto2.Idade = 24;
-dto2.Cargo = "Programador Backend";
-dto2.Cpf = "11111111111111";
-
-//False(quando classe) pois eles tem ponteiros apontando para endereços diferente na memória Heap
-//True(quando record)  porque os records são projetados para comparar seus valores em vez de suas referências de memória.
-Console.WriteLine(dto == dto2);
+FormularioDto dto = new FormularioDto("Gabriel", "11111111111111", "Programador Backend") { Idade = 24};
+//dto.Nome = "Gabriel"; Imutável
+dto.Idade = 100; // Mutável
+Console.WriteLine($"Valor do nome: {dto.Nome}");
