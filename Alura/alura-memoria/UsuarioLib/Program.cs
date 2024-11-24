da8056ec1e94f73fd5eebde1b1384f04273287f8
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-24 16:38:26 -0300

    Records e equalidade

--- Alura/alura-memoria/UsuarioLib/Program.cs
+++ Alura/alura-memoria/UsuarioLib/Program.cs
@@ -6,7 +6,21 @@ Usuario usuario =
         "daniel@email.com",
         new List<string>() {"12345678"});
 
-FormularioDto dto = new FormularioDto("Gabriel", "11111111111111", "Programador Backend") { Idade = 24};
-//dto.Nome = "Gabriel"; Imutável
-dto.Idade = 100; // Mutável
-Console.WriteLine($"Valor do nome: {dto.Nome}");
+UsuarioDto dto1 = new UsuarioDto();
+dto1.Nome = "Daniel";
+dto1.Email = "daniel@email.com";
+//dto1.Telefones = new List<string>();
+dto1.Telefones = null;
+
+UsuarioDto dto2 = new UsuarioDto();
+dto2.Nome = "Daniel";
+dto2.Email = "daniel@email.com";
+//dto2.Telefones = new List<string>();
+dto2.Telefones = null;
+
+// False
+// O tipo lista é um tipo de referência, ao instanciarmos uma lista ela aponta para endereços de memória diferentes,
+// por conta disso, a comparação da false
+
+// E ao definirmos a lista de telefone como nulo os records "voltam" a serem iguais
+Console.WriteLine(dto1==dto2);
