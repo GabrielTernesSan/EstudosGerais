26af2750147325ff19d23cad7e13c6ec6d66b44a
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-25 07:25:21 -0300

    Acessando propriedades dinamicamente

--- Alura/csharp-reflection/ByteBank.Common/LeitorDeBoleto.cs
+++ Alura/csharp-reflection/ByteBank.Common/LeitorDeBoleto.cs
@@ -1,4 +1,6 @@
-﻿namespace ByteBank.Common
+﻿using System.Reflection;
+
+namespace ByteBank.Common
 {
     public class LeitorDeBoleto
     {
@@ -36,19 +38,31 @@
         private T MapearTextoParaObjeto<T>(string[] nomesPropriedades, string[] valoresPropriedades)
         {
             T instancia = Activator.CreateInstance<T>();
-            //instancia.CedenteNome = valoresPropriedades[0];
-            //instancia.CedenteCpfCnpj = valoresPropriedades[1];
-            //instancia.CedenteAgencia = valoresPropriedades[2];
-            //instancia.CedenteConta = valoresPropriedades[3];
-            //instancia.SacadoNome = valoresPropriedades[4];
-            //instancia.SacadoCpfCnpj = valoresPropriedades[5];
-            //instancia.SacadoEndereco = valoresPropriedades[6];
-            //instancia.Valor = Convert.ToDecimal(valoresPropriedades[7]);
-            //instancia.DataVencimento = Convert.ToDateTime(valoresPropriedades[8]);
-            //instancia.NumeroDocumento = valoresPropriedades[9];
-            //instancia.NossoNumero = valoresPropriedades[10];
-            //instancia.CodigoBarras = valoresPropriedades[11];
-            //instancia.LinhaDigitavel = valoresPropriedades[12];
+
+            // Percorre os nomes das propriedades
+            for (int i = 0; i < nomesPropriedades.Length; i++)
+            {
+                // Obtém a propriedade atual através do nome
+                string nomePropriedade = nomesPropriedades[i];
+                PropertyInfo propertyInfo = instancia.GetType().GetProperty(nomePropriedade);
+
+                // Verificar se a propriedade foi encontrada
+                if (propertyInfo != null)
+                {
+                    // Obtém o tipo da propriedade
+                    Type propertyType = propertyInfo.PropertyType;
+
+                    // Obtém o valor da propriedade
+                    string valor = valoresPropriedades[i];
+
+                    // Converte o valor da propriedade para o tipo correto
+                    Object valorConvertido = Convert.ChangeType(valor, propertyType);
+
+                    // Guarda o valor convertidop na propriedade
+                    propertyInfo.SetValue(instancia, valorConvertido);
+                }
+            }
+
             return instancia;
         }
     }
