11e411116f5f23498ea584696272843e7dc41500
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-11-27 06:27:52 -0300

    Padrão AAA

--- Alura/csharptestes-jornadamilhas/README.md
+++ Alura/csharptestes-jornadamilhas/README.md
@@ -37,3 +37,19 @@ O Assert possui vários métodos que nos auxiliam a fazer essas verificações e
 - Assert.Empty: Verifica se uma coleção está vazia.
 - Assert.StartsWith: Verifica se uma string começa com determinado prefixo.
 - Assert.EndsWith: Verifica se uma string termina com determinado sufixo.
+
+### O Padrão Triple A (Arrange, Act, Assert)
+
+O modelo Triple A é um padrão que diz que todo o teste de unidade deve possuir três etapas: Arrange (Preparar o teste), Act (Rodar o teste) e Assert (Verificar as asserções).
+
+#### 1ª Arrange
+
+Nesta etapa nós configuramos tudo o que é necessário para que o nosso teste possa rodar, inicializamos variáveis, criamos alguns test doubles como Mocks ou Spies dentre outras coisas.
+
+#### 2ª Act
+
+Esta etapa é onde rodamos de fato o nosso teste. Chamamos alguma função ou método que queremos por a prova.
+
+#### 3ª Assert
+
+Esta etapa é onde faremos nosso assert. É onde verificamos se a operação realizada na etapa anterior (Act) surtiu o resultado esperado. Assim sabemos se o teste passa ou falha.
--- Alura/csharptestes-jornadamilhas/test/JornadaMilhas.Test/OfertaViagemTest.cs
+++ Alura/csharptestes-jornadamilhas/test/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -7,14 +7,30 @@ namespace JornadaMilhas.Test
         [Fact]
         public void TestandoOfertaValida()
         {
+            // Cenário - Arrange
             Rota rota = new Rota("OrigemTeste", "DestinoTeste");
             Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
             double preco = 100.0;
             var validacao = true;
 
+            // Ação - Act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+            // Validação - Assert
             Assert.Equal(validacao, oferta.EhValido);
+        }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
         }
     }
 }
