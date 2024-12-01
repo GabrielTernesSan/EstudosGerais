65ad6955e2400c08d0d90b65e2977026616be463
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-12-01 20:15:50 -0300

    Criando um teste para desconto

---
+++ Alura/csharptestes-jornadamilhas/test/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,26 @@
+﻿using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 05, 5), new DateTime(2024, 05, 10));
+            double precoOriginal = 100.0;
+            double desconto = 20.0;
+            double precoComDesconto = precoOriginal - desconto;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // Act
+            oferta.Desconto = desconto;
+
+            // Assert
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+    }
+}
