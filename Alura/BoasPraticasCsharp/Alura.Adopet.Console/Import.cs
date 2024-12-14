c28722bd4405c9c658b7720c1cff7830af98485a
GabrielTernesSan <santos.tgabriel@hotmail.com>
2024-12-14 14:05:20 -0300

    Código duplicado na leitura de arquivo

--- Alura/BoasPraticasCsharp/Alura.Adopet.Console/Import.cs
+++ Alura/BoasPraticasCsharp/Alura.Adopet.Console/Import.cs
@@ -14,25 +14,9 @@ namespace Alura.Adopet.Console
 
         public async Task ImportacaoArquivoPetAsync(string caminhoDoArquivoDeImportacao)
         {
-            List<Pet> listaDePet = new List<Pet>();
+            var leitor = new LeitorDeArquivo();
 
-            using (StreamReader sr = new StreamReader(caminhoDoArquivoDeImportacao))
-            {
-                System.Console.WriteLine("----- Dados importados -----");
-                while (!sr.EndOfStream)
-                {
-                    // separa linha usando ponto e vírgula
-                    string[] propriedades = sr.ReadLine().Split(';');
-                    // cria objeto Pet a partir da separação
-                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                      propriedades[1],
-                      TipoPet.Cachorro
-                     );
-
-                    System.Console.WriteLine(pet);
-                    listaDePet.Add(pet);
-                }
-            }
+            List<Pet> listaDePet = leitor.RealizaLeitura(caminhoDoArquivoDeImportacao);
 
             foreach (var pet in listaDePet)
             {
