855b73a453523816ddd4be373c2f0f3e148a2bcc
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-01 19:35:09 -0300

    Desafio

--- Alura/BoasPraticasCsharp/Alura.Adopet.Console/Program.cs
+++ Alura/BoasPraticasCsharp/Alura.Adopet.Console/Program.cs
@@ -1,31 +1,17 @@
 ﻿using Alura.Adopet.Console.Comandos;
 
-Dictionary<string, IComando> comandosDoSistema = new()
-{
-    { "help", new Help() },
-    { "import", new Import() },
-    { "list", new List() },
-    { "show", new Show() }
-};
+ComandosDoSistema comandos = new();
 
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
     string comando = args[0].Trim();
-
-    if (comandosDoSistema.ContainsKey(comando))
-    {
-        IComando? cmd = comandosDoSistema[comando];
-        await cmd.ExecutarAsync(args);
-    }
-    else
-    {
-        Console.WriteLine("Comando inválido!");
-    }
+    IComando? cmd = comandos[comando];
+    if (cmd is not null) await cmd.ExecutarAsync(args);
+    else Console.WriteLine("Comando inválido!");
 }
 catch (Exception ex)
 {
-    // mostra a exceção em vermelho
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"Aconteceu um exceção: {ex.Message}");
 }
