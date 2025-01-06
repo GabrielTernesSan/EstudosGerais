fa5a1e75ec2d164c926a6dd5260d7b73bb857f3e
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-06 11:51:25 -0300

    Desafios

--- Alura/BoasPraticasCsharp/Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
+++ Alura/BoasPraticasCsharp/Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
@@ -6,7 +6,7 @@ namespace Alura.Adopet.Testes
     public class PetAPartirDoCsvTest
     {
         [Fact]
-        public void QuandoStringForValidaDeveRetornarUmPet()
+        public void QuandoStringForValidaDeveRetornatUmPet()
         {
             //Arrange
             string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
@@ -16,6 +16,46 @@ namespace Alura.Adopet.Testes
 
             //Assert
             Assert.NotNull(pet);
+        }
+
+        [Fact]
+        public void QuandoStringForNulaDeveLancarArgumentNullException()
+        {
+            //Arrange
+            string? linha = null;
+
+            //Act + Assert
+            Assert.Throws<ArgumentNullException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoStringForVaziaDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = string.Empty;
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoGuidForInvalidoDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = "aksjdha;Nina;2";
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
+        }
+
+        [Fact]
+        public void QuandoTipoForInvalidoDeveLancarArgumentException()
+        {
+            //Arrange
+            string? linha = "609c9b0d-aa02-459f-a340-256513fc9bad;Nina;3";
+
+            //Act + Assert
+            Assert.Throws<ArgumentException>(() => linha.ConverteDoTexto());
         }
     }
 }
