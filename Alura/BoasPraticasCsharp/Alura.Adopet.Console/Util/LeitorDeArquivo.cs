d03c9b8bfe02ec77a82fc128dd7a56930773f7d8
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-06 11:37:27 -0300

    Nova refatoração com testes

--- Alura/BoasPraticasCsharp/Alura.Adopet.Console/Util/LeitorDeArquivo.cs
+++ Alura/BoasPraticasCsharp/Alura.Adopet.Console/Util/LeitorDeArquivo.cs
@@ -12,15 +12,7 @@ namespace Alura.Adopet.Console.Util
                 System.Console.WriteLine("----- Serão importados os dados abaixo -----");
                 while (!sr.EndOfStream)
                 {
-                    // separa linha usando ponto e vírgula
-                    string[] propriedades = sr.ReadLine().Split(';');
-
-                    // cria objeto Pet a partir da separação
-                    Pet pet = new Pet(
-                        Guid.Parse(propriedades[0]),
-                        propriedades[1],
-                        TipoPet.Cachorro
-                    );
+                    var pet = sr.ReadLine().ConverteDoTexto();
 
                     listaDePet.Add(pet);
 
