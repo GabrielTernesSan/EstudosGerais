46b59a5f634c0384d756cb3c2244b30cb48a1138
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-07 09:06:57 -0300

    ClassFixture

--- Alura/jornadaMilhas-curso2/JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
+++ Alura/jornadaMilhas-curso2/JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
@@ -1,21 +1,17 @@
 using JornadaMilhas.Dados;
 using JornadaMilhasV1.Modelos;
-using Microsoft.EntityFrameworkCore;
 using Xunit.Abstractions;
 
 namespace JornadaMilhas.Test.Integracao;
 
-public class OfertaViagemDalAdicionar
+public class OfertaViagemDalAdicionar : IClassFixture<ContextoFixture>
 {
     private readonly JornadaMilhasContext context;
 
-    public OfertaViagemDalAdicionar(ITestOutputHelper output)
+    public OfertaViagemDalAdicionar(ITestOutputHelper output, ContextoFixture fixture)
     {
-        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
-            .Options;
+        context = fixture.Context;
 
-        context = new JornadaMilhasContext(options);
         output.WriteLine(context.GetHashCode().ToString());
     }
 
