7a4d04a89b75a48426fb68956876e3176804245f
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-08 13:10:21 -0300

    Desafio

---
+++ Alura/jornadaMilhas-curso2/JornadaMilhas.Test.Integracao/OfertaViagemDalRecuperarTodas.cs
@@ -0,0 +1,31 @@
+﻿using JornadaMilhas.Dados;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao
+{
+    [Collection(nameof(ContextoCollection))]
+    public class OfertaViagemDalRecuperarTodas
+    {
+        private readonly JornadaMilhasContext context;
+
+        public OfertaViagemDalRecuperarTodas(ITestOutputHelper output, ContextoFixture fixture)
+        {
+            context = fixture.Context;
+
+            output.WriteLine(context.GetHashCode().ToString());
+        }
+
+        [Fact]
+        public void RetornaListaQuandoHaOfertas()
+        {
+            // Arrange
+            var dal = new OfertaViagemDAL(context);
+
+            // Act
+            var ofertas = dal.RecuperarTodas();
+
+            // Assert
+            Assert.NotEmpty(ofertas);
+        }
+    }
+}
