2de8732c421fc789f235802eb94c594f866948ff
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-11 17:35:05 -0300

    Testando API fora do ar

--- Alura/BoasPraticasCsharp2/Alura.Adopet.Testes/HttpClientPetTest.cs
+++ Alura/BoasPraticasCsharp2/Alura.Adopet.Testes/HttpClientPetTest.cs
@@ -3,6 +3,7 @@ using Alura.Adopet.Console.Servicos;
 using Moq;
 using Moq.Protected;
 using System.Net;
+using System.Net.Sockets;
 
 namespace Alura.Adopet.Testes
 {
@@ -81,11 +82,22 @@ namespace Alura.Adopet.Testes
         public async Task QuandoAPIForaDeveRetornarUmaExcecao()
         {
             //Arrange
-            //var clientePet = new HttpClientPet(uri:"http://localhost:1111");
+            var handlerMock = new Mock<HttpMessageHandler>();
+
+            handlerMock
+               .Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+               .ThrowsAsync(new SocketException());
+
+            var httpClient = new Mock<HttpClient>(MockBehavior.Default, handlerMock.Object);
+            httpClient.Object.BaseAddress = new Uri("http://localhost:5057");
+            var clientePet = new HttpClientPet(httpClient.Object);
 
             ////Act+Assert
-            //await Assert.ThrowsAnyAsync<Exception>(()=> clientePet.ListPetsAsync());
-
+            await Assert.ThrowsAnyAsync<Exception>(() => clientePet.ListPetsAsync());
         }
 
     }
