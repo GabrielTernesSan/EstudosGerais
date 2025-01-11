c8a771382871cea83cde66255aacb4918f76774b
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-01-11 17:10:55 -0300

    IHttpClientFactory

---
+++ Alura/BoasPraticasCsharp2/Alura.Adopet.Console/Comandos/List.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos;
+
+namespace Alura.Adopet.Console.Comandos
+{
+    [DocComandoAttribute(instrucao: "list",
+      documentacao: "adopet list comando que exibe no terminal o conteúdo cadastrado na base de dados da AdoPet.")]
+    internal class List: IComando
+    {
+        private readonly HttpClientPet _clientPet;
+
+        public List(HttpClientPet clientPet)
+        {
+            _clientPet = clientPet;
+        }
+
+        public Task ExecutarAsync(string[] args)
+        {
+            return this.ListaDadosPetsDaAPIAsync();
+        }
+
+        private async Task ListaDadosPetsDaAPIAsync()
+        {
+            IEnumerable<Pet>? pets = await _clientPet.ListPetsAsync();
+            System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+    }
+}
---
+++ Alura/BoasPraticasCsharp2/Alura.Adopet.Console/Servicos/AdopetAPIClientFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+
+namespace Alura.Adopet.Console.Servicos
+{
+    internal class AdopetAPIClientFactory : IHttpClientFactory
+    {
+        private string uri = "http://localhost:5057";
+        public HttpClient CreateClient(string name)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(uri);
+            return _client;
+        }
+    }
+}
