e88fe6ba5ad72a4c0b53942dc4a7d9cea30eed79
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-06-05 09:27:46 -0300

    Desafio, login com erro

---
+++ JornadaMilhasAPIDockerTest/test/JornadaMilhas.Integration.Test.API/JornadaMilhas_AuthTest.cs
@@ -0,0 +1,42 @@
+﻿using JornadaMilhas.API.DTO.Auth;
+using System.Net.Http.Json;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class JornadaMilhas_AuthTest
+    {
+        [Fact]
+        public async Task POST_Efetua_Login_Com_Sucesso()
+        {
+            //Arrange
+            var app = new JornadaMilhasWebApplicationFactory();
+
+            var user = new UserDTO { Email = "tester@email.com", Password = "Senha123@" };
+
+            using var client = app.CreateClient();
+
+            //Act
+            var resultado = await client.PostAsJsonAsync("/auth-login", user);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, resultado.StatusCode);
+        }
+
+        [Fact]
+        public async Task POST_Login_Falha_User_E_Senha_Invalidos()
+        {
+            //Arrange
+            var app = new JornadaMilhasWebApplicationFactory();
+
+            var user = new UserDTO { Email = "gabriel@", Password = "123" };
+
+            using var client = app.CreateClient();
+
+            //Act
+            var resultado = await client.PostAsJsonAsync("/auth-login", user);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, resultado.StatusCode);
+        }
+    }
+}
