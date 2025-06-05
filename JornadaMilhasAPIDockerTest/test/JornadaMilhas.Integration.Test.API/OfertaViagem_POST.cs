6cd6b0c17702a8a4f2550854b6a776173a62d869
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-06-05 09:54:31 -0300

    Testando a inclusão de ofertas

---
+++ JornadaMilhasAPIDockerTest/test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -0,0 +1,29 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System.Net.Http.Json;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class OfertaViagem_POST
+    {
+        [Fact]
+        public async Task Cadastra_OfertaViagem()
+        {
+            // Arrange
+            var app = new JornadaMilhasWebApplicationFactory();
+
+            using var client = app.CreateClient();
+            
+            var ofertaViagem = new
+            {
+                Preco = 100,
+                Rota = new Rota("Origem", "Destino"),
+                Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+            };
+            // Act
+            var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
