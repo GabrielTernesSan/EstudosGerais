f27aa6cd53fc3f0c6c0d60babaea7bd0744b0ba4
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-06-29 16:24:50 -0300

    update

--- JornadaMilhasAPIDockerTest/test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
+++ JornadaMilhasAPIDockerTest/test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -1,4 +1,8 @@
-﻿namespace JornadaMilhas.Integration.Test.API
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System.Net.Http.Json;
+
+namespace JornadaMilhas.Integration.Test.API
 {
     public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory>
     {
@@ -13,8 +17,32 @@
         public async Task Recupera_OfertaViagem_PorId()
         {
             // Arrange
+            var ofertaExistente = app.Context.OfertasViagem.FirstOrDefault();
+
+            if (ofertaExistente is null)
+            {
+                ofertaExistente = new OfertaViagem()
+                {
+                    Preco = 100,
+                    Rota = new Rota("Origem", "Destino"),
+                    Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+                };
+
+                app.Context.Add(ofertaExistente);
+
+                app.Context.SaveChanges();
+            }
+
+            using var client = await app.GetClientWithAccessTokenAsync();
+
             // Act
+            var response = await client.GetFromJsonAsync<OfertaViagem>($"/ofertas-viagem/{ofertaExistente.Id}");
+
             // Assert
+            Assert.NotNull(response);
+            Assert.Equal(ofertaExistente.Preco, response.Preco, 0.001);
+            Assert.Equal(ofertaExistente.Rota.Origem, response.Rota.Origem);
+            Assert.Equal(ofertaExistente.Rota.Destino, response.Rota.Destino);
         }
     }
 }
