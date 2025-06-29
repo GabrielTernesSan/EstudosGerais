793057f6c1f9c0d5e9f5895c31091b38102a1c1b
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-06-29 14:11:53 -0300

    Expondo o contexto

---
+++ JornadaMilhasAPIDockerTest/test/JornadaMilhas.Integration.Test.API/OfertaViagem_GET.cs
@@ -0,0 +1,20 @@
+﻿namespace JornadaMilhas.Integration.Test.API
+{
+    public class OfertaViagem_GET : IClassFixture<JornadaMilhasWebApplicationFactory>
+    {
+        private readonly JornadaMilhasWebApplicationFactory app;
+
+        public OfertaViagem_GET(JornadaMilhasWebApplicationFactory app)
+        {
+            this.app = app;
+        }
+
+        [Fact]
+        public async Task Recupera_OfertaViagem_PorId()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
